80a1a5498c5105a3bfaa1aa868664ff362d4a446
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2023-06-26 18:40:18 +0200

    Stworzenie projektu

---
+++ jaz24965nbp/src/main/java/com/example/jaz24965nbp/Jaz24965nbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz24965nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz24965nbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz24965nbpApplication.class, args);
+    }
+
+}
---
+++ jaz24965nbp/src/test/java/com/example/jaz24965nbp/Jaz24965nbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz24965nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz24965nbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
